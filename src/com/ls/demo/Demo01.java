48b5bf643b8a2e94451dca6a46a0dc5b24131355
HakunaMatataBert <Bert.zhang30@outlook.com>
2018-11-29 15:30:45 +0800

    ds

--- src/com/ls/demo/Demo01.java
+++ src/com/ls/demo/Demo01.java
@@ -5,6 +5,8 @@ public class Demo01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.err.println("测定文件demo");
+		
+		System.err.println("github");
 	}
 
 }
